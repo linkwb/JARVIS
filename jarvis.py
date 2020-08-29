a1fd561226c7c05060d000f9d30681cfb5629f62
wbryden <linkwsb@gmail.com>
2020-08-29 15:42:04 -0400

    Remove text prompts and add voice prompts.

--- jarvis.py
+++ jarvis.py
@@ -1,10 +1,9 @@
-import random
 import datetime
-import re
 import json
 import urllib.request
 import pyowm
 import pyttsx3
+import speech_recognition as sr
 
 owm = pyowm.OWM('7451715cb6e843730e4c2c5c894788cc')
 url = 'http://ipinfo.io/json'
@@ -18,17 +17,50 @@ time = datetime.datetime.now()
 engine = pyttsx3.init()
 rate = engine.getProperty('rate')
 engine.setProperty('rate', rate+50)
+r = sr.Recognizer()
+mic = sr.Microphone()
 
-#initial prompt
-print("Hello, Sir. The time is "+time.strftime("%H:%M")+". How can I be of assistance?\n")
-engine.say("Hello, Sir. The Time is "+time.strftime("%H:%M")+". How can I be of assistance?")
+
+def recognize_speech_from_mic(r, mic):
+    if not isinstance(r, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(mic, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    with mic as source:
+        print("Speak:")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    try:
+        response["transcription"] = r.recognize_google(audio)
+    except sr.RequestError:
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+
+# initial prompt
+print("Hello, William. The time is "+time.strftime("%H:%M")+". How can I be of assistance?\n")
+engine.say("Hello, William. The Time is "+time.strftime("%H:%M")+". How can I be of assistance?")
 engine.runAndWait()
-prompt = input("> ")
+speech = recognize_speech_from_mic(r, mic)
+prompt = speech["transcription"]
 
-#begin while loop
-while "exit" not in prompt:
+# begin while loop
+while "shut down" not in prompt:
     
-    #time
+    # time
     if "time" in prompt:
         print("The time is "+time.strftime("%H:%M")+".")
         engine.say("The time is "+time.strftime("%H:%M")+".")
@@ -37,9 +69,10 @@ while "exit" not in prompt:
         print("How else might I be of assistance?\n")
         engine.say("How else might I be of assistance?")
         engine.runAndWait()
-        prompt = input("> ")
+        speech = recognize_speech_from_mic(r, mic)
+        prompt = speech["transcription"]
 
-    #date
+    # date
     elif "date" in prompt:
         print("The date is "+time.strftime("%m-%d-%y")+".")
         engine.say("The date is "+time.strftime("%m-%d-%y")+".")
@@ -48,9 +81,10 @@ while "exit" not in prompt:
         print("How else might I be of assistance?\n")
         engine.say("How else might I be of assistance?")
         engine.runAndWait()
-        prompt = input("> ")
+        speech = recognize_speech_from_mic(r, mic)
+        prompt = speech["transcription"]
 
-    #location
+    # location
     elif "location" in prompt:
         print("Your current location is "+city+", "+region+".")
         engine.say("Your current location is "+city+", "+region+".")
@@ -59,23 +93,29 @@ while "exit" not in prompt:
         print("How else might I be of assistance?\n")
         engine.say("How else might I be of assistance?")
         engine.runAndWait()
-        prompt = input("> ")
+        speech = recognize_speech_from_mic(r, mic)
+        prompt = speech["transcription"]
 
-    #weather
+    # weather
     elif "weather" in prompt:
-        print("The weather in "+city+", "+region+" is "+str(weather.get_temperature('fahrenheit')['temp']) +" degrees Fahrenheit.")
-        engine.say("The weather in "+city+", "+region+" is "+str(weather.get_temperature('fahrenheit')['temp'])+" degrees Fahrenheit.")
+        print("The weather in " + city + ", " + region + " is " + str(weather.get_temperature('fahrenheit')['temp']) + " degrees Fahrenheit.")
+        engine.say("The weather in " + city + ", " + region + " is " + str(weather.get_temperature('fahrenheit')['temp']) + " degrees Fahrenheit.")
         engine.runAndWait()
 
         print("How else might I be of assistance?\n")
         engine.say("How else might I be of assistance?")
         engine.runAndWait()
-        prompt = input("> ")
+        speech = recognize_speech_from_mic(r, mic)
+        prompt = speech["transcription"]
 
-    #error
+    # error
     else:
-        print("Sorry. I seem to be malfunctioning. Please try again.\n")
-        prompt = input("> ")
+        print("Sorry. I seem to be malfunctioning. What was that?\n")
+        engine.say("Sorry. I seem to be malfunctioning. What was that?\n")
+        engine.runAndWait()
+
+        speech = recognize_speech_from_mic(r, mic)
+        prompt = speech["transcription"]
 
 print("I'll get back to running calculations, Sir.\n")
 engine.say("I'll get back to running calculations.")
